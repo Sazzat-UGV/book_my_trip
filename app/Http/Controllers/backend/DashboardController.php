1ae5bf04efd4d9834dafd8c6458940f63c70a40f
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2023-10-20 00:40:40 +0600

    added admin dashboard content

--- app/Http/Controllers/backend/DashboardController.php
+++ app/Http/Controllers/backend/DashboardController.php
@@ -2,8 +2,15 @@
 
 namespace App\Http\Controllers\backend;
 
+use App\Models\User;
+use App\Models\Hotel;
+use App\Models\Order;
+use App\Models\Flight;
+use App\Models\Package;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Contact;
 use Illuminate\Support\Facades\Gate;
 
 class DashboardController extends Controller
@@ -11,6 +18,25 @@ class DashboardController extends Controller
     public function dashboard()
     {
         Gate::authorize('access-dashboard');
-        return view('backend.pages.dashboard');
+        $category = Category::count();
+        $packages = Package::count();
+        $flights = Flight::count();
+        $hotels = Hotel::count();
+        $users = User::where('role_id', 3)->count();
+        $orders = Order::count();
+        $contact = Contact::count();
+        $systemAdmins = User::whereNot('role_id', 3)->count();
+        $totalIncome = Order::sum('amount');
+        return view('backend.pages.dashboard', compact(
+            'category',
+            'packages',
+            'flights',
+            'hotels',
+            'users',
+            'orders',
+            'contact',
+            'systemAdmins',
+            'totalIncome',
+        ));
     }
 }
--- resources/views/backend/pages/dashboard.blade.php
+++ resources/views/backend/pages/dashboard.blade.php
@@ -5,9 +5,143 @@
 @push('admin_style')
 @endpush
 @section('content')
-    @include('backend.layout.inc.breadcump', ['page_name' => 'Dashboard'])
     <div class="row">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati eum veniam asperiores error modi dignissimos, cumque nihil consectetur eaque tenetur!
+        <div class="container-fluid">
+            @include('backend.layout.inc.breadcump', ['page_name' => 'Dashboard'])
+
+            <!-- Content Row -->
+            <div class="row">
+
+
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        Category</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $category }}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-warning shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        Packages</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $packages }}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-danger shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        Flights</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $flights }}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-success shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        Hotels</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $hotels }}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-info shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        Registered User</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $users }}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-dark shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        System Admin</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $systemAdmins }}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-info shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        Total Order</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $orders }}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-secondary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        Contact</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $contact }}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-warning shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        Order Profit</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalIncome }} BDT</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
 @endsection
 @push('admin_script')
